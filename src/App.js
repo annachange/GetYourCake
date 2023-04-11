5bf013717c883b7338b1706a9ed4156b991c7d19
Anna <marhel.anna@gmail.com>
2023-04-11 11:13:07 +0300

    deactivated user login

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import NavbarFirstPage from './components/NavbarFirstPage';
-import UserLogin from './UserLogin';
+//import UserLogin from './UserLogin';
 
 import { BrowserRouter as Router } from "react-router-dom";
 
@@ -17,7 +17,7 @@ function App() {
           {/* Put your page content here */}
           <div className="content section">
               <NavbarFirstPage/>
-              <UserLogin/>
+              {/*<UserLogin/>*/}
             <img src="/gyk_logo.png" className="App-logo" alt="logo" />
             <p>Ordering a cake is a piece of cake</p>
           </div>
