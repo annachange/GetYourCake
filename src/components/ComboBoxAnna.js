c9307bbfa9f1ed2dadf6007085a6a6cc21d14790
Anna <marhel.anna@gmail.com>
2023-04-14 20:34:42 +0300

    dsfs

--- src/components/ComboBoxAnna.js
+++ src/components/ComboBoxAnna.js
@@ -1,33 +1,19 @@
 import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-import Radio from '@mui/material/Radio';
+
 import { Grid, CardMedia, CardHeader, Accordion, AccordionSummary, AccordionDetails, Card, CardContent, Typography } from '@mui/material';
-import FormGroup from '@mui/material/FormGroup';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import Switch from '@mui/material/Switch';
-import CardActions from '@mui/material/CardActions';
+
 import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
-import { red } from '@mui/material/colors';
-import IconButton from '@mui/material/IconButton';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import Rating from '@mui/material/Rating';
+
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import Slider from '@mui/material/Slider';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 
 import CakeFlavoursSelection from "./CakeFlavoursSelection";
-import textpage from "./textpage";
+
 
 
 import img1 from '../images/image1.jpg';
@@ -89,7 +75,7 @@ function ComboBoxAnna(props) {
             value: 5,
             label: '5',
 
-},
+        },
 
 
 
@@ -128,145 +114,149 @@ function ComboBoxAnna(props) {
     return (
 
         <>
-        <div className="container">
-        <Grid container rowSpacing={2} justify="center">
-            <Grid item xs={6} >
-                <Box
-                    borderRadius={10}
-                    border={4}
-                    borderColor="#cca2a2"
-                    p={2}
-                    sx={{
-                        width: '100%',
-                        maxWidth: 600,
-                        boxShadow: '0px 20px 20px 0px rgba(0,0,0,0.25)',
-                    }}
-                    style={{ backgroundColor: '#FFFFFF' }}
-                >
-                    <Card sx={{ maxWidth: 345 }} elevation={0}>
-                        <CardMedia
-                            component="img"
-                            image={props.cake.picture_path}
-                            alt={props.cake.name}
+            <div className="container">
+                <Grid container rowSpacing={2} justify="center">
+                    <Grid item xs={6} >
+                        <Box
+                            borderRadius={10}
+                            border={4}
+                            borderColor="#cca2a2"
+                            p={2}
+                            sx={{
+                                width: '100%',
+                                maxWidth: 600,
+                                boxShadow: '0px 20px 20px 0px rgba(0,0,0,0.25)',
+                                backgroundColor: '#FFFFFF',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center'
+                            }}
+                        >
+                            <Card sx={{ maxWidth: 345 }} elevation={0}>
+                                <CardMedia
+                                    component="img"
+                                    image={props.cake.picture_path}
+                                    alt={props.cake.name}
+                                    sx={{ display: 'flex', justifyContent: 'center' }}
 
-                        />
-                    </Card>
-                </Box>
-                <Box
-                    borderRadius={10}
-                    border={4}
-                    borderColor="#cca2a2"
-                    p={2}
-                    sx={{
-                        width: '100%',
-                        maxWidth: 600,
-                        boxShadow: '0px 20px 20px 0px rgba(0,0,0,0.25)',
-                    }}
-                    style={{ backgroundColor: '#FFFFFF' }}
-                >
+                                />
+                            </Card>
+                        </Box>
+                        <Box
+                            borderRadius={10}
+                            border={4}
+                            borderColor="#cca2a2"
+                            p={2}
+                            sx={{
+                                width: '100%',
+                                maxWidth: 600,
+                                boxShadow: '0px 20px 20px 0px rgba(0,0,0,0.25)',
+                            }}
+                            style={{ backgroundColor: '#FFFFFF' }}
+                        >
 
 
-                    <VerticalTabs baker={props.baker} cake={props.cake}/>
-                </Box>
+                            <VerticalTabs baker={props.baker} cake={props.cake}/>
+                        </Box>
 
+                    </Grid>
+                    <Grid item xs={6} >
+                        <Box
+                            borderRadius={10}
+                            border={4}
+                            borderColor="#cca2a2"
+                            p={2}
+                            sx={{
+                                width: '100%',
+                                maxWidth: 600,
+                                boxShadow: '0px 20px 20px 0px rgba(0,0,0,0.25)',
+                            }}
+                            style={{ backgroundColor: '#FFFFFF' }}
+                        >
+
+                            <div>
+                                <Typography variant="h4">{props.cake.name}</Typography>
+
+                            </div>
+                            <Divider sx={{ marginBottom: "15px", marginTop: "15px" }}>
+                                <Chip style={{ backgroundColor: '#cca2a2', color: 'white',  width: '200px', height: '25px' }} size="large" label="CAKE SIZE (KG)" />
+                            </Divider>
+                            <div className="form-section" sx={{ width: 300 }}>
+                                <Slider
+                                    aria-label="Cake sixe"
+                                    defaultValue={2}
+                                    getAriaValueText={valuetext}
+                                    valueLabelDisplay="auto"
+                                    step={0.2}
+                                    marks
+                                    min={0.6}
+                                    max={5.4}
+                                    color="secondary"
+                                    sx={{width: 400}}
+                                    marks={marks}
+
+                                    onChange={(event, value) => setSelectedCakeSize(value)}
+                                />
+                            </div>
+
+                            <Divider sx={{ marginBottom: "15px", marginTop: "15px" }}>
+                                <Chip style={{ backgroundColor: '#cca2a2', color: 'white',  width: '200px', height: '25px' }} size="large" label="FLAVOUR" />
+                            </Divider>
+                            <div className="form-section">
+
+                                <CakeFlavoursSelection />
+
+
+
+
+
+
+
+
+                            </div>
+                            <Divider sx={{ marginBottom: "15px", marginTop: "15px" }}>
+                                <Chip style={{ backgroundColor: '#cca2a2', color: 'white',  width: '200px', height: '25px' }} size="large" label="DELIVERY" />
+                            </Divider>
+
+                            <div className="form-section">
+
+
+
+                                <SwitcherTest />
+
+
+
+                            </div>
+                            <Divider sx={{ marginBottom: "15px", marginTop: "15px" }}>
+                                <Chip style={{ backgroundColor: '#cca2a2', color: 'white',  width: '200px', height: '25px' }} size="large" label="COMMENT" />
+                            </Divider>
+
+
+                            <div className="form-section">
+
+                                <TextField
+                                    sx={{width: 400}}
+                                    justify="center"
+                                    id="standard-multiline-static"
+                                    label="Please add comment"
+                                    multiline
+                                    rows={2}
+                                    variant="standard"
+                                />
+                            </div>
+
+                            <div>
+                                <Typography variant="h4">TOTAL PRICE IS {calculatePrice()} EUR</Typography>
+                            </div>
+
+                            <div> <Button>Add to cart</Button></div>
+
+                        </Box>
+                    </Grid>
                 </Grid>
-                <Grid item xs={6} >
-                    <Box
-                        borderRadius={10}
-                        border={4}
-                        borderColor="#cca2a2"
-                        p={2}
-                        sx={{
-                            width: '100%',
-                            maxWidth: 600,
-                            boxShadow: '0px 20px 20px 0px rgba(0,0,0,0.25)',
-                        }}
-                        style={{ backgroundColor: '#FFFFFF' }}
-                    >
-
-                    <div>
-                        <Typography variant="h4">{props.cake.name}</Typography>
-
-                    </div>
-                        <Divider sx={{ marginBottom: "15px", marginTop: "15px" }}>
-                            <Chip style={{ backgroundColor: '#cca2a2', color: 'white',  width: '200px', height: '25px' }} size="large" label="CAKE SIZE (KG)" />
-                        </Divider>
-                    <div className="form-section" sx={{ width: 300 }}>
-                        <Slider
-                            aria-label="Cake sixe"
-                            defaultValue={2}
-                            getAriaValueText={valuetext}
-                            valueLabelDisplay="auto"
-                            step={0.2}
-                            marks
-                            min={0.6}
-                            max={5.4}
-                            color="secondary"
-                            sx={{width: 400}}
-                            marks={marks}
-
-                            onChange={(event, value) => setSelectedCakeSize(value)}
-                        />
-                    </div>
-
-                        <Divider sx={{ marginBottom: "15px", marginTop: "15px" }}>
-                            <Chip style={{ backgroundColor: '#cca2a2', color: 'white',  width: '200px', height: '25px' }} size="large" label="FLAVOUR" />
-                        </Divider>
-                    <div className="form-section">
-
-<CakeFlavoursSelection />
 
 
-
-
-
-
-
-
-                    </div>
-                        <Divider sx={{ marginBottom: "15px", marginTop: "15px" }}>
-                            <Chip style={{ backgroundColor: '#cca2a2', color: 'white',  width: '200px', height: '25px' }} size="large" label="DELIVERY" />
-                        </Divider>
-
-                    <div className="form-section">
-
-
-
-                        <SwitcherTest />
-
-
-
-                    </div>
-                        <Divider sx={{ marginBottom: "15px", marginTop: "15px" }}>
-                            <Chip style={{ backgroundColor: '#cca2a2', color: 'white',  width: '200px', height: '25px' }} size="large" label="COMMENT" />
-                        </Divider>
-
-
-                    <div className="form-section">
-
-                        <TextField
-                            sx={{width: 400}}
-                            justify="center"
-                            id="standard-multiline-static"
-                            label="Please add comment"
-                            multiline
-                            rows={2}
-                            variant="standard"
-                        />
-                    </div>
-
-                        <div>
-                            <Typography variant="h4">TOTAL PRICE IS {calculatePrice()} EUR</Typography>
-                        </div>
-
-                    <div> <Button>Add to cart</Button></div>
-
-                    </Box>
-                </Grid>
-            </Grid>
-
-
-</div>
+            </div>
 
         </>
     );
